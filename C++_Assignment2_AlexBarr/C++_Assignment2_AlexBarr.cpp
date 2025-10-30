0cf1458378512c2025c8eff69cb03f28fc6d922f
apb-nscc <the.alchemist.coder@gmail.com>
2025-10-30 14:17:33 -0300

    Assignment 2 - Initial Complete (Will Review and make a new Commit If Needed)

--- C++_Assignment2_AlexBarr/C++_Assignment2_AlexBarr.cpp
+++ C++_Assignment2_AlexBarr/C++_Assignment2_AlexBarr.cpp
@@ -16,49 +16,64 @@ using namespace std;
 class Student
 {
 private:
-    string name;         ///< Student's name
-    int numCourses;      ///< Number of courses
-    string* courseList;  ///< Dynamic array of course names
+    string name;         /// Student's name
+    int numCourses;      /// Number of courses
+    string* courseList;  /// Dynamic array of course names
+	int studentNumber;   /// Unique student identifier
 
 public:
     /// Default constructor
     Student()
-        : name(""), numCourses(0), courseList(nullptr)
+        : name(""), numCourses(0), courseList(nullptr), studentNumber(0)
     {
     }
 
     /// Constructor with name
     Student(const string& studentName)
-        : name(studentName), numCourses(0), courseList(nullptr)
+        : name(studentName), numCourses(0), courseList(nullptr), studentNumber(0)
     {
-        printf("\x1B[93m[String constructor] Created student with name='%s'\033[0m\t\t\n",name.c_str());
+        printf("\x1B[93m[String constructor] Created student with name='%s' (Student %d)\033[0m\t\t\n", name.c_str(), studentNumber);
     }
 
-    /// Constructor with name and course count
-    Student(const string& studentName, int courses)
-        : name(studentName), numCourses(courses), courseList(nullptr)
+	/// Constructor with name and student number
+    Student(const string& studentName, int number)
+        : name(studentName), numCourses(0), courseList(nullptr), studentNumber(number)
+    {
+        printf("\x1B[93m[String constructor] Created student with name='%s' (Student %d)\033[0m\t\t\n", name.c_str(), studentNumber);
+    }
+
+	/// Constructor with name and course count and student number
+    Student(const string& studentName, int courses, int number)
+        : name(studentName), numCourses(courses), courseList(nullptr), studentNumber(number)
     {
         if (numCourses > 0)
-            courseList = new string[numCourses];
+        {
+			courseList = new string[numCourses]; // Does not need to be a pointer here because we just need to make sure this variable is the right size
+        }
     }
 
-    /// Constructor with name, course count, and course list
-    Student(const string& studentName, int size, const string* courseList)
-        : name(studentName), numCourses(size), courseList(nullptr)
+	/// Constructor with name, course count, and course list and student number
+    Student(const string& studentName, int size, const string* courseList, int number)
+        : name(studentName), numCourses(size), courseList(nullptr), studentNumber(number)
     {
         if (numCourses > 0 && courseList != nullptr)
         {
             this->courseList = new string[numCourses];
-            for (int i = 0; i < numCourses; ++i)
-                this->courseList[i] = courseList[i]; // deep copy
+
+            for (int i = 0; i < numCourses; i++)
+            {
+				this->courseList[i] = courseList[i]; // Deep copy of course names
+            }
+                
         }
     }
 
     /// Copy constructor
     Student(const Student& other)
-        : name(other.name), numCourses(other.numCourses), courseList(nullptr)
+        : name(other.name), numCourses(other.numCourses), courseList(nullptr), studentNumber(other.studentNumber)
     {
-        printf("\x1B[93m[Copy constructor] Copying student '%s' (%i courses) \033[0m\t\t\n", other.name.c_str(), other.numCourses);
+        printf("\x1B[93m[Copy constructor] Copying student '%s' (Student %d, %i courses) \033[0m\t\t\n", other.name.c_str(), other.studentNumber, other.numCourses);
+        
         if (numCourses > 0)
         {
             courseList = new string[numCourses];
@@ -72,37 +87,38 @@ public:
     /// Assignment operator (deep copy, exception-safe)
     Student& operator=(const Student& other)
     {
-        if (this == &other) {
-            printf("\x1B[93m[Assign operator] Self-assignment detected for '%s'\033[0m\t\t\n", name.c_str());
+        if (this == &other) 
+        {
+            printf("\x1B[93m[Assign operator] Self-assignment detected for '%s' (Student %d)\033[0m\t\t\n", name.c_str(), studentNumber);
             return *this;
         }
 
-        // Allocate and copy first (may throw) so we don't modify 'this' on failure
         string* newList = nullptr;
-        if (other.numCourses > 0) {
+        if (other.numCourses > 0) 
+        {
             newList = new string[other.numCourses];
-            for (int i = 0; i < other.numCourses; ++i)
+
+            for (int i = 0; i < other.numCourses; i++)
+            {
                 newList[i] = other.courseList[i];
+            }  
         }
 
         string newName = other.name;
 
-        // Commit changes
         delete[] courseList;
         courseList = newList;
         numCourses = other.numCourses;
-        name = std::move(newName);
+        name = newName;
 
-        //cout << "[Assign op] Assigned from '" << other.name << "' (" << other.numCourses << " courses) to '" << name << "'\n";
-        printf("\x1B[93m[Assign operator] Assigned from '%s' (%i courses) to '%s'\033[0m\t\t\n", other.name.c_str(), other.numCourses, name.c_str());
+        printf("\x1B[93m[Assign operator] Assigned from '%s' (Student %d, %i courses) to '%s' (Student %d)\033[0m\t\t\n", other.name.c_str(), other.studentNumber, other.numCourses, name.c_str(), studentNumber);
         return *this;
     }
 
     /// Destructor
     ~Student()
     {
-        //cout << "[Destructor] Destroying student '" << name << "'\n";
-        printf("\x1B[91m[Destructor] Destroying student '%s'\033[0m\t\t\n", name.c_str());
+        printf("\x1B[91m[Destructor] Destroying student '%s' (Student %d)\033[0m\t\t\n", name.c_str(), studentNumber);
         delete[] courseList;
         courseList = nullptr;
     }
@@ -121,22 +137,27 @@ public:
         newList[numCourses] = course;
         delete[] courseList;
         courseList = newList;
-        ++numCourses;
+        numCourses++;
     }
 
     /// Print student details
     void print() const
     {
-        printf("\x1B[93m[print] Called for student '%s'\033[0m\t\t\n", name.c_str());
-        cout << "Student Name: " << name << endl;
+        printf("\x1B[93m[print] Called for student '%s' (Student %d)\033[0m\t\t\n", name.c_str(), studentNumber);
+        cout << "Student Name: " << name << " (Student " << studentNumber << ")" << endl;
         cout << "Number of Courses: " << numCourses << endl;
         cout << "Courses: \n";
+        
         for (int i = 0; i < numCourses; i++)
         {
             if (i == numCourses - 1)
-                cout << "\t" << courseList[i] << " ";
+            {
+				cout << "\t" << courseList[i] << " ";
+            }
             else
+            {
                 cout << "\t" << courseList[i] << ",\n";
+            }
         }
         cout << endl;
     }
@@ -144,29 +165,31 @@ public:
     /// Overloaded << operator for Student
     friend ostream& operator<<(ostream& os, const Student& student)
     {
-        printf("\x1B[93m[operator<<] stream operator called for student '%s'\033[0m\t\t\n", student.name.c_str());
-        os << "Student Name: " << student.name << "\n";
+        printf("\x1B[93m[operator<<] stream operator called for student '%s' (Student %d)\033[0m\t\t\n", student.name.c_str(), student.studentNumber);
+        
+        os << "Student Name: " << student.name << " (Student " << student.studentNumber << ")\n";
         os << "Number of Courses: " << student.numCourses << "\n";
         os << "Courses: \n";
+        
         for (int i = 0; i < student.numCourses; i++)
         {
             if (i == student.numCourses - 1)
+            {
                 os << "\t" << student.courseList[i] << " ";
+            }
             else
+            {
                 os << "\t" << student.courseList[i] << ",\n";
+            }
         }
         return os;
     }
 
-	/// Overloaded << operator to assign one Student to another... I understand this is probably bad practice, but I want to show this works
- //   friend Student& operator<<(Student& student1, Student& student2)
- //   {
- //       student1 = student2;
- //       return student1;
-	//}
-
     /// Friend function to change student name
     friend void changeName(Student& student, const string& newName);
+
+	/// Friend function to set student number
+	friend void setNumber(Student& student, int newNumber);
 
     /// Reset all courses for the student
     void resetCourses()
@@ -182,16 +205,22 @@ void changeName(Student& student, const string& newName)
     student.name = newName;
 }
 
+void setNumber(Student& student, int newNumber)
+{
+    student.studentNumber = newNumber;
+}
+
 void resetCourses(Student& student)
 {
     student.resetCourses();
 }
 
-Student AskForSecondStudent(int& numCourses,Student& s1)
+Student AskForSecondStudent(int& numCourses,Student& s1, int studentNumber)
 {
     string name = enterName();
     Student s2(s1);
     changeName(s2, name);
+	setNumber(s2, studentNumber); // Increment student number for second student
 
     return s2;
 }
@@ -201,12 +230,12 @@ void displayStudent(const Student& s)
     s.print();
 }
 
-Student testStudentClass(int &numCourses)
+Student testStudentClass(int &numCourses, int studentNumber)
 {
     string name = enterName();
     // enterCourses returns a dynamically allocated array and sets numCourses
     string* courses = enterCourses(numCourses);
-    Student s1(name, numCourses, courses);
+    Student s1(name, numCourses, courses, studentNumber);
 
     // Student made a deep copy; free the temporary array
     delete[] courses;
@@ -220,13 +249,15 @@ string enterName()
     string name;
     while (true)
     {
-        try {
+        try 
+        {
             cout << "Enter new student name: ";
             getline(cin, name);
             cout << "\n" << endl;
             break;
         }
-        catch (const exception& e) {
+        catch (const exception& e) 
+        {
             cerr << "Incorrect Input: " << e.what() << endl;
         }
     }
@@ -239,13 +270,15 @@ string enterCourse()
     string course;
     while (true)
     {
-        try {
+        try 
+        {
             cout << "Enter course name: ";
             getline(cin, course);
             cout << "\n" << endl;
             break;
         }
-        catch (const exception& e) {
+        catch (const exception& e) 
+        {
             cerr << "Incorrect Input: " << e.what() << endl;
         }
     }
@@ -258,7 +291,8 @@ string* enterCourses(int& numCourses) {
     string course;
     string choice;
 
-    while (true) {
+    while (true) 
+    {
         cout << "Enter a new course? (Y or N): ";
         getline(cin, choice);
 
@@ -297,18 +331,22 @@ string* enterCourses(int& numCourses) {
 
 int main()
 {
+	int studentCounter = 1;
     char restart = 'N';
+
     do
     {
         int numCourses = 0;
         printf("\x1B[94mInput first Student info \n-----------------------\n\n\033[0m");
-        Student firstStudent = testStudentClass(numCourses);
+        Student firstStudent = testStudentClass(numCourses, studentCounter);
+		studentCounter++;
 
         printf("\x1B[94m\n Display first Student info \n-----------------------\n\n\033[0m");
         displayStudent(firstStudent);
 
         printf("\x1B[94m\n Create Second Student\n-----------------------\n\n\033[0m");
-        Student secondStudent = AskForSecondStudent(numCourses, firstStudent);
+        Student secondStudent = AskForSecondStudent(numCourses, firstStudent, studentCounter);
+		studentCounter++;
 
         printf("\x1B[94m\n Display Second Student\n-----------------------\n\n\033[0m");
         displayStudent(secondStudent);
@@ -322,9 +360,9 @@ int main()
 
         Student thirdStudent;
         thirdStudent = secondStudent; // Using overloaded << operator to assign secondStudent to thirdStudent
+		setNumber(thirdStudent, studentCounter); // Increment student number for third student
 
         printf("\x1B[94m\nThird student after assignment (third = second):\n------------------------------------------------\n\033[0m");
-        //displayStudent(thirdStudent);
         cout << "\n" << thirdStudent; // Using overloaded << operator to print thirdStudent
 
         string restartInput;
@@ -332,12 +370,17 @@ int main()
         getline(cin, restartInput);
 
         if (restartInput == "y" || restartInput == "Y")
+        {
             restart = 'Y';
+        } 
         else
+        {
             restart = 'N';
+        }
     } while (restart == 'y' || restart == 'Y');
 
     cout << "Program exiting.\n";
+	studentCounter = 1; // Reset student counter for potential future runs
 
     return 0;
 }
